9e38ee5f3e7dbad7f29147cdf9855d79d468012f
julianpodruhin <yulianpodruhin@mail.ru>
2023-02-27 14:18:27 +0300

    Lab 1 ex 5

---
+++ src/Programm.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Programm
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число 1");
+        int num_1 = in.nextInt();
+        System.out.println(num_1);
+        System.out.println("Введите число 2");
+        int num_2 = in.nextInt();
+        System.out.println(num_2);
+        System.out.println("Введите число 3");
+        int num_3 = in.nextInt();
+        System.out.println(num_3 );
+    }
+}
